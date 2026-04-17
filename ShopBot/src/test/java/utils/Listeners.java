89b4462f0bb7aa337bbc1b197ee1d77c0143e542
bhavya <bhavyasree.kasa@gmail.com>
2026-04-17 14:35:35 +0530

    Test Module -5 completed extent report file creation

--- ShopBot/src/test/java/utils/Listeners.java
+++ ShopBot/src/test/java/utils/Listeners.java
@@ -6,19 +6,45 @@ import org.openqa.selenium.WebDriver;
 
 import base.BaseTest;
 
+import com.aventstack.extentreports.*;
+import com.aventstack.extentreports.MediaEntityBuilder;
+
 public class Listeners implements ITestListener {
+
+    ExtentReports extent = ExtentManager.getInstance();
+    ExtentTest test;
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        test = extent.createTest(result.getName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.pass("Test Passed");
+    }
 
     @Override
     public void onTestFailure(ITestResult result) {
 
-        WebDriver driver = BaseTest.getDriver(); 
+        WebDriver driver = BaseTest.getDriver();
 
-        if (driver != null) {
-            String testName = result.getName();
-            ScreenshotUtil.captureScreenshot(driver, testName);
-            System.out.println("Screenshot captured for failed test: " + testName);
-        } else {
-            System.out.println("Driver is null, screenshot not captured");
+        String testName = result.getName();
+
+        String path = ScreenshotUtil.captureScreenshot(driver, testName);
+
+        test.fail("Test Failed");
+
+        try {
+            test.fail("Screenshot",
+                    MediaEntityBuilder.createScreenCaptureFromPath(path).build());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    @Override
+    public void onFinish(org.testng.ITestContext context) {
+        extent.flush();
     }
 }
